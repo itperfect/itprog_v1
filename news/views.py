646c72f81e0a705c5db706f25a40e4c72c8cb5a0
Artyom Salkov <artyom.salkov@gmail.com>
2021-03-22 16:55:42 +0200

    functional for form "add article"

--- news/views.py
+++ news/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
 
 
 def news_home(request):
-    news = Articles.objects.order_by('-date')[:2]
+    news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
 
 
 def create(request):
-    return render(request, 'news/create.html')
+
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='home')
+        else:
+            error = 'Форма была неверной'
+
+    form = ArticlesForm()
+
+    data = {
+        'form': form,
+        'error': error
+    }
+
+    return render(request, 'news/create.html', data)
