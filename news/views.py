1694a38c5955880d92158a8ae02d9f104bf8cbed
Artyom Salkov <artyom.salkov@gmail.com>
2021-03-22 14:34:04 +0200

    add form "add article" w/o handler

--- news/views.py
+++ news/views.py
@@ -3,5 +3,9 @@ from .models import Articles
 
 
 def news_home(request):
-    news = Articles.objects.all()
+    news = Articles.objects.order_by('-date')[:2]
     return render(request, 'news/news_home.html', {'news': news})
+
+
+def create(request):
+    return render(request, 'news/create.html')
