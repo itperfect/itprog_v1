11054b83d2b432e4b591fabc29d43202e102eeb1
Artyom Salkov <artyom.salkov@gmail.com>
2021-03-26 11:57:26 +0200

    Added requirements.txt

--- reporting/views.py
+++ reporting/views.py
@@ -2,21 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.conf import settings
+# from django.conf import settings
+from django.db import connections
 
 
 def index(request):
-    data = settings.DATABASES
-
+    # data = settings.DATABASES
     res = {}
+    cn = ''
 
-    for conn_name, conn_data in data.items():
-        tmp = {}
-        for param_name, param_val in conn_data.items():
-            if param_name in ('NAME', 'USER', 'PASSWORD', "HOST", 'PORT'):
-                tmp[param_name] = param_val
-        res[conn_name] = tmp
+    m_obj = connections.databases
+    conn_dict = m_obj.keys()
 
-    data = res
+    for conn_name in conn_dict:
+        db_conn = connections[conn_name]
+        try:
+            c = db_conn.cursor()
+            print(f'Connection to database {db_conn} is OK')
+            c.execute('select count(*) from auth_permission')
+            d = c.fetchone()
+            print(d)
+            cn = 'OK'
+        except Exception as e:
+            print(f'Connection to database {db_conn} is ERROR')
+            cn = 'ERROR'
+        finally:
+            res[db_conn] = cn
 
-    return render(request, 'reporting/index.html', {'data': data})
+    return render(request, 'reporting/index.html', {'data': res, 'models': conn_dict})
